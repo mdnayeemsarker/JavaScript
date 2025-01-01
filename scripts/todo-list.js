a28353918ed0334ddbbfe30143a0f0df17d32e89
mdnayeemsarker <dev.ab.nayeem@gmail.com>
2025-01-01 17:55:23 +0600

    basic project of todo list

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,16 +1,37 @@
-// const todoList = [];
+const todoList = [
+    'Learn HTML',
+    'Learn CSS',
+    'Learn JavaScript',
+    'Learn PHP',
+    'Learn Python',
+    'Learn Ruby',
+    'Learn Java',
+    'Learn C++',
+    'Learn C#',
+    'Learn Swift'
+];
 
-// function addTodo() {
-//   const inputElement = document.querySelector('.todo-name');
-//   const name = inputElement.value;
-//   console.log(name);
 
-//   todoList.push(name);
+function renderTodoList() {
+    let todoHtml = '';
 
-//   console.log(todoList);
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `<li>${todo}</li>`;
+        todoHtml += html;
+    }
 
-//   inputElement.value = ''; 
-    
-// }
+    document.querySelector('.todo-list').innerHTML = todoHtml;
+}
+
+renderTodoList(); // the main idea of javascript of 3 things: save data, generate html, and render html also interactive website.
+
+function addTodo() {
+  const inputElement = document.querySelector('.todo-name');
+  const name = inputElement.value;
+  todoList.push(name);
+  inputElement.value = ''; 
+  renderTodoList();    
+}
 
 
