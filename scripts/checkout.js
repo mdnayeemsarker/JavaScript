2652a56e14bece490cd278fdb0a8b1abb8618758
mdnayeemsarker <dev.ab.nayeem@gmail.com>
2025-01-02 19:54:02 +0600

    remove container

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
         }
     });
     cartSummaryHTML +=
-    `<div class="cart-item-container">
+    `<div class="cart-item-container js-cart-item-container-${productFound.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -99,6 +99,9 @@ document.querySelectorAll('.js-dilit-link').forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeProduct(productId);
+        // const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        // container.remove();
+        document.querySelector(`.js-cart-item-container-${productId}`).remove();
         // console.log(cart); 
     });
 });
