f8904f4275e11ab85bd12cd5d3fb3a097b0822b0
mdnayeemsarker <dev.ab.nayeem@gmail.com>
2025-01-04 01:55:33 +0600

    inherite paramiter and method

--- data/products.js
+++ data/products.js
@@ -31,8 +31,20 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return'';
+  }
 }
 class Clothing extends Product {
+  sizeChartLink;
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+  extraInfoHTML() {
+    return `<a href="${this.sizeChartLink}" target="_blank">Size Chart</a>`;
+  }
 }
 
 const tshirt = new Clothing({
@@ -716,4 +728,9 @@ export const products = [
       "mens"
     ]
   }
-].map((productDetails) => new Product(productDetails));
+].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);    
+  }
+  return new Product(productDetails)
+});
