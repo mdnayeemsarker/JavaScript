c39fb5e0ce9a248b97729e7ebb57c46147cad427
mdnayeemsarker <dev.ab.nayeem@gmail.com>
2025-01-04 14:11:29 +0600

    setup basic backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummery } from './checkout/orderSummary.js';
 import { renderPaymentSummery } from './checkout/paymentSummary.js';
+import '../data/backend.js'
 renderOrderSummery();
 
 renderPaymentSummery();
