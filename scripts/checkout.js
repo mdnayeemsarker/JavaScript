0ed2eddbe75f64011ccbbe310de5389696515a92
mdnayeemsarker <dev.ab.nayeem@gmail.com>
2025-01-04 15:29:57 +0600

    basic promice

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,11 @@ import { renderPaymentSummery } from './checkout/paymentSummary.js';
 // import '../data/backend.js'
 import { loadProducts } from '../data/products.js';
 
+
+new Promise((result) =>{
+    console.log('new Promise');
+})
+
 loadProducts(() => {
     renderOrderSummery();
     renderPaymentSummery();
