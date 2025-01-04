8b2b70a3b206cf51e7ffe5811a82ab116a0c1759
mdnayeemsarker <dev.ab.nayeem@gmail.com>
2025-01-04 14:49:58 +0600

    backend setup real url

--- data/backend.js
+++ data/backend.js
@@ -2,5 +2,5 @@ const xhr = new XMLHttpRequest();
 xhr.addEventListener('load', ()=>{
     console.log(xhr.response);    
 });
-xhr.open('GET', 'https://backedn.com');
+xhr.open('GET', 'https://juktoshop.com/api/v1/app-home');
 xhr.send();
